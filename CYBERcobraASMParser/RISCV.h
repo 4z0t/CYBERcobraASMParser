a7338fcd2075e8e62a4c0fb56fae650857bc5737
Alexander <84857900+4z0t@users.noreply.github.com>
2023-05-04 10:15:02 +0300

    wip transformers

--- CYBERcobraASMParser/RISCV.h
+++ CYBERcobraASMParser/RISCV.h
@@ -7,7 +7,7 @@
 namespace riscv
 {
 	using std::string;
-	using std::vector;
+	using std::make_unique;
 	using std::unique_ptr;
 	using namespace CYBERCobra;
 
@@ -56,7 +56,25 @@ namespace riscv
 	private:
 	};
 
-	class DirectTransformer : public InstructionTransformer
+	class JumpTransformer :public virtual InstructionTransformer
+	{
+	public:
+		virtual CYBERCobraInstruction Transform(Instruction const& instr)
+		{
+		}
+
+	};
+
+	class LoadImmediateTransformer :public virtual InstructionTransformer
+	{
+	public:
+		virtual CYBERCobraInstruction Transform(Instruction const& instr)
+		{
+		}
+
+	};
+
+	class DirectTransformer : public virtual InstructionTransformer
 	{
 	public:
 
@@ -68,10 +86,23 @@ namespace riscv
 		virtual CYBERCobraInstruction Transform(Instruction const& instr)
 		{
 		}
-	private:
+	protected:
 		ALUOP m_op;
 	};
 
+	class BranchTransformer : public DirectTransformer
+	{
+	public:
+
+		BranchTransformer(ALUOP op)
+			:DirectTransformer(op)
+		{
+		}
+
+		virtual CYBERCobraInstruction Transform(Instruction const& instr)
+		{
+		}
+	};
 
 
 	using InstructionTransformer_ptr = unique_ptr<InstructionTransformer>;
@@ -80,7 +111,7 @@ namespace riscv
 	{
 	public:
 		InstructionParser() {};
-		InstructionParser(Operation op, string const& s, InstructionTransformer_ptr transformer)
+		InstructionParser(Operation op, string const& s, InstructionTransformer_ptr&& transformer)
 			: m_op(op), m_s(s), m_transformer(std::move(transformer))
 		{
 		};
@@ -93,26 +124,26 @@ namespace riscv
 	};
 
 
-	const vector<InstructionParser> parsers
+	const InstructionParser parsers[]
 	{
-		{Operation::ADD, "add", std::make_unique<DirectTransformer>(ALUOP::ALU_ADD)},
-		{Operation::SUB, "sub", std::make_unique<DirectTransformer>(ALUOP::ALU_SUB)},
-		{Operation::XOR, "xor", std::make_unique<DirectTransformer>(ALUOP::ALU_XOR)},
-		{Operation::OR,  "or",  std::make_unique<DirectTransformer>(ALUOP::ALU_OR)},
-		{Operation::AND, "and", std::make_unique<DirectTransformer>(ALUOP::ALU_AND)},
-		{Operation::SLL, "sll", std::make_unique<DirectTransformer>(ALUOP::ALU_SLL)},
-		{Operation::SRL, "srl", std::make_unique<DirectTransformer>(ALUOP::ALU_SRL)},
-		{Operation::SRA, "sra", std::make_unique<DirectTransformer>(ALUOP::ALU_SRA)},
-		{Operation::SLT, "slt", std::make_unique<DirectTransformer>(ALUOP::ALU_SLTS)},
-		{Operation::SLTU,"sltu",std::make_unique<DirectTransformer>(ALUOP::ALU_SLTU)},
-		{Operation::LI,  "li",  nullptr},
-		{Operation::J,   "j",   nullptr},
-		{Operation::BEQ, "beq", std::make_unique<DirectTransformer>(ALUOP::ALU_EQ)},
-		{Operation::BNE, "bne", std::make_unique<DirectTransformer>(ALUOP::ALU_NE)},
-		{Operation::BLT, "blt", std::make_unique<DirectTransformer>(ALUOP::ALU_LTS)},
-		{Operation::BGE, "bge", std::make_unique<DirectTransformer>(ALUOP::ALU_GES)},
-		{Operation::BLTU,"bltu",std::make_unique<DirectTransformer>(ALUOP::ALU_LTU)},
-		{Operation::BGEU,"bgeu",std::make_unique<DirectTransformer>(ALUOP::ALU_GEU)},
+		{Operation::ADD, "add", make_unique<DirectTransformer>(ALUOP::ALU_ADD)},
+		{Operation::SUB, "sub", make_unique<DirectTransformer>(ALUOP::ALU_SUB)},
+		{Operation::XOR, "xor", make_unique<DirectTransformer>(ALUOP::ALU_XOR)},
+		{Operation::OR,  "or",  make_unique<DirectTransformer>(ALUOP::ALU_OR)},
+		{Operation::AND, "and", make_unique<DirectTransformer>(ALUOP::ALU_AND)},
+		{Operation::SLL, "sll", make_unique<DirectTransformer>(ALUOP::ALU_SLL)},
+		{Operation::SRL, "srl", make_unique<DirectTransformer>(ALUOP::ALU_SRL)},
+		{Operation::SRA, "sra", make_unique<DirectTransformer>(ALUOP::ALU_SRA)},
+		{Operation::SLT, "slt", make_unique<DirectTransformer>(ALUOP::ALU_SLTS)},
+		{Operation::SLTU,"sltu",make_unique<DirectTransformer>(ALUOP::ALU_SLTU)},
+		{Operation::LI,  "li",  make_unique<LoadImmediateTransformer>()},
+		{Operation::J,   "j",   make_unique<JumpTransformer>()},
+		{Operation::BEQ, "beq", make_unique<BranchTransformer>(ALUOP::ALU_EQ)},
+		{Operation::BNE, "bne", make_unique<BranchTransformer>(ALUOP::ALU_NE)},
+		{Operation::BLT, "blt", make_unique<BranchTransformer>(ALUOP::ALU_LTS)},
+		{Operation::BGE, "bge", make_unique<BranchTransformer>(ALUOP::ALU_GES)},
+		{Operation::BLTU,"bltu",make_unique<BranchTransformer>(ALUOP::ALU_LTU)},
+		{Operation::BGEU,"bgeu",make_unique<BranchTransformer>(ALUOP::ALU_GEU)},
 	};
 
 
